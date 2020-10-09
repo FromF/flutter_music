2a7cb8efc1c35f819a99efe23ba0ddfa3771ec25
Haruhito Fuji <haruhito.fuji@gmail.com>
2020-10-09 16:49:40 +0900

    development music app

--- flutter_music/lib/main.dart
+++ flutter_music/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'music app',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'music app'),
     );
   }
 }
@@ -50,68 +52,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // シンバル用Audio
+  final cymbalPlayer = AudioCache(fixedPlayer: AudioPlayer());
+  AudioPlayer cymbalAudioSource;
+  // ギター用Audio
+  final guitarPlayer = AudioCache(fixedPlayer: AudioPlayer());
+  AudioPlayer guitarAudioSource;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Image.asset(
+            'images/background.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.fitHeight,
+          ),
+          SafeArea(child:
+          Center(child:
+            Row(
+                //横幅いっぱいを指定する
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  FlatButton(onPressed: () async {
+                    if (cymbalAudioSource != null) {
+                      // すでに再生中なら一旦停止する
+                      cymbalAudioSource.stop();
+                    }
+                    // AudioCacheを用いて再生
+                    cymbalAudioSource = await cymbalPlayer.play('cymbalSound.mp3', mode: PlayerMode.LOW_LATENCY);
+
+                  },
+                    child: Image.asset('images/cymbal.png')
+                  ),
+                  FlatButton(onPressed: () async {
+                    if (guitarAudioSource != null) {
+                      // すでに再生中なら一旦停止する
+                      guitarAudioSource.stop();
+                    }
+                    // AudioCacheを用いて再生
+                    guitarAudioSource = await guitarPlayer.play('guitarSound.mp3', mode: PlayerMode.LOW_LATENCY);
+                  },
+                      child: Image.asset('images/guitar.png')
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
